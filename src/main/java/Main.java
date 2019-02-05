093351665969a5f864dd392accd7fd5039c0754b
Shane Archuleta <shane.archuleta17@ncf.edu>
2019-02-05 10:00:52 -0500

    Load main.fxml and display as main window

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,32 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+            Parent parent = loader.load();
+
+            Scene scene = new Scene(parent);
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, e.getMessage());
+            alert.show();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
